b9f715f3ec1e3c4b508df55c6a8b362d2f21bdf1
Hathor Hamilton <koshknot@gmail.com>
2021-09-13 13:09:30 -0400

    html prep

---
+++ index.js
@@ -0,0 +1,11 @@
+//Monster Card Display Properties
+    //Name
+    //Image
+    //Armor Class
+    //HitPoints
+    //Action
+
+
+// so when we fect the info from the API, we need to add a click event that takes the fetched information and feeds it into the monster-card div
+// we will make a function to call at the end of the fetch that does all this 
+// hardcode a monster to load in with the HTML so that there is always visible content
